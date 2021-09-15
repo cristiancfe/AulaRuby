89f3fa73ce19e5cfac7f7461b82c22443661890d
cristiancfe <cristiancfe@gmail.com>
2021-09-14 22:14:32 -0300

    testando Push remote

---
+++ Ruby_aulas.rb
@@ -0,0 +1,37 @@
+#puts ("hello world")
+
+=begin
+nome = "cristian"
+puts (" bem vindo #{nome}")
+=end
+
+=begin
+x,y = 10,20
+puts (x+y)
+=end
+
+=begin
+x = 20.0 # divisão por ornto flutuante 
+z = 10.0
+y = "cristian"
+puts (x/z)
+puts (x+z)
+puts (x%z)
+puts (x-z)
+puts (x*z)
+puts (x**2)
+puts y * 3 \
+puts 5-2*3+3
+=end
+
+puts ("digite seu nome")
+nome = gets.chomp
+puts ("seu nome é #{nome}")
+
+
+
+
+
+
+
+
